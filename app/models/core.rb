f713628524fc367a58d4f2a1280c3fd8978ffb1e
pingnamo <gary@mona.co>
2020-09-30 19:08:33 +0800

    ✨ Slippage curve

--- app/models/core.rb
+++ app/models/core.rb
@@ -15,27 +15,72 @@ class Core
     end
 
     def swap(from, to, amount)
+      message = "\n"
+
       fa = Asset.find(from)
       ta = Asset.find(to)
 
-      price = Oracle.price(from, to)
+      message += "From: #{from}(cash: #{fa.cash}, liabilities: #{fa.liabilities})\n"
+      message += "To: #{to}(cash: #{ta.cash}, liabilities: #{ta.liabilities})\n"
+      message += "From Amount: #{amount}\n"
 
-      received = amount * price
-      ta.debit(received)
+      price = Oracle.price(from, to)
+      from_amount = amount
+      to_amount = amount * price
+
+      message += "==============================\n"
+      message += "Oracle Price: #{price}\n"
+      message += "To Amount: #{to_amount}\n"
+
+      raise Asset::NotEnoughCash if ta.cash < to_amount
+
+      from_before_cov = fa.cash / fa.liabilities
+      to_before_cov = ta.cash / ta.liabilities
+      from_after_cov = (fa.cash + from_amount) / fa.liabilities
+      to_after_cov = (ta.cash - to_amount) / ta.liabilities
+
+      message += "==============================\n"
+      message += "From Cov (Before): #{from_before_cov}\n"
+      message += "From Cov (After): #{from_after_cov}\n"
+      message += "To Cov (Before): #{to_before_cov}\n"
+      message += "To Cov (After): #{to_after_cov}\n"
+
+      from_s = (s(from_after_cov) - s(from_before_cov)) / (from_after_cov - from_before_cov)
+      to_s = (s(to_after_cov) - s(to_before_cov)) / (to_after_cov - to_before_cov)
+
+      sft = to_s - from_s
+
+      message += "Si: #{from_s}\n"
+      message += "Sj: #{to_s}\n"
+      message += "Sj - Si: #{sft}\n"
+
+      adjusted_price = price * (1 + sft)
+      adjusted_to_amount = amount * adjusted_price
+
+      message += "==============================\n"
+      message += "Adjusted Price: #{adjusted_price}\n"
+      message += "Adjusted To Amount: #{adjusted_to_amount}\n"
+
+      ta.debit(adjusted_to_amount)
       fa.credit(amount)
 
       # TODO: Transfer to account
 
-      received
+      message
     end
 
-    def slippage_curve(x)
-      p1 = 3
-      p2 = 200
-      if x.zero?
-        0.0
-      else
-        1 + 1 / (p2**(x**p1) - 1)
+    def s(x)
+      a = 0.3
+      b = 0.9
+
+      if x >= 0 && a > x
+        -x + (a + b) / 2
+      elsif x >= a && b > x
+        (x - b)**2 / 2 / (b - a)
+      elsif x >= b
+        0
+      else # x < 0 ??
+        0
       end
     end
 
@@ -44,7 +89,7 @@ class Core
 
       message += "============== Assets ==============\n"
       Core.pool.assets.each do |asset|
-        message += "#{asset.token_id.value}(cash: #{asset.cash}, liabilities: #{asset.liabilities})"
+        message += "#{asset.token_id.value}(cash: #{asset.cash}, liabilities: #{asset.liabilities}, coverage: #{asset.cov})"
         message += "\n"
       end
 
